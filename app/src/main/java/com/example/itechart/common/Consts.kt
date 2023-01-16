6bb7a37939ed3705e7cd1380c83eab1a2ab652d8
Vazha <vazha@Administrators-MacBook-Pro.local>
2023-01-16 16:41:38 +0400

    implemented retrofit instance in home module

---
+++ app/src/main/java/com/example/itechart/common/Consts.kt
@@ -0,0 +1,5 @@
+package com.example.itechart.common
+
+object Consts {
+    const val BASE_URL = "don_have_yet"
+}
--- app/src/main/java/com/example/itechart/home_screen/di/HomeModule.kt
+++ app/src/main/java/com/example/itechart/home_screen/di/HomeModule.kt
@@ -1,4 +1,28 @@
 package com.example.itechart.home_screen.di
 
+import com.example.itechart.common.Consts.BASE_URL
+import com.example.itechart.home_screen.data.source.remote.HomeServiceApi
+import com.squareup.moshi.Moshi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object HomeModule {
+
+    @Provides
+    @Singleton
+    fun provideHomeApi(okHttpClient: OkHttpClient, moshi: Moshi): HomeServiceApi =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(HomeServiceApi::class.java)
 }

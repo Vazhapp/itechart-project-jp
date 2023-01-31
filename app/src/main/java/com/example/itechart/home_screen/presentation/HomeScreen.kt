46ee21d8223e21b7fb62a4c8bcea7a3370fb0091
Vazha <vazha@Administrators-MacBook-Pro.local>
2023-02-01 00:43:00 +0400

    add root box on home screen ui

--- app/src/main/java/com/example/itechart/home_screen/presentation/HomeScreen.kt
+++ app/src/main/java/com/example/itechart/home_screen/presentation/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.example.itechart.home_screen.presentation
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
@@ -8,7 +9,9 @@ import com.example.itechart.home_screen.presentation.ui_components.Profile
 
 @Composable
 fun HomeScreen() {
-    Column(modifier = Modifier.fillMaxSize()) {
-        Profile()
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            Profile()
+        }
     }
 }

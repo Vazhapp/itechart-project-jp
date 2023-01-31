d6ecfd2f5d20ec3c2edf30e21ff693769ec6dd6e
Vazha <vazha@Administrators-MacBook-Pro.local>
2023-02-01 00:41:58 +0400

    put profile composable on home screen

--- app/src/main/java/com/example/itechart/home_screen/presentation/HomeScreen.kt
+++ app/src/main/java/com/example/itechart/home_screen/presentation/HomeScreen.kt
@@ -4,10 +4,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.itechart.home_screen.presentation.ui_components.Profile
 
 @Composable
 fun HomeScreen() {
     Column(modifier = Modifier.fillMaxSize()) {
-
+        Profile()
     }
 }

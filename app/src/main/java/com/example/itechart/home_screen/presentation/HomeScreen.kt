9c43c7bce758b78515759c2167a5904b134ade3f
Vazha <vazha@Administrators-MacBook-Pro.local>
2023-01-31 16:20:26 +0400

    implemented profile ui for home screen

--- app/src/main/java/com/example/itechart/home_screen/presentation/HomeScreen.kt
+++ app/src/main/java/com/example/itechart/home_screen/presentation/HomeScreen.kt
@@ -1,8 +1,13 @@
 package com.example.itechart.home_screen.presentation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun HomeScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
 
+    }
 }

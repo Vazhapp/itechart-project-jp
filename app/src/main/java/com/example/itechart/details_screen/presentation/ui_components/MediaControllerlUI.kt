e1b80bb220bbca5e99ef7d28d6ca16c7fb08405c
Vazhapp <vkentchiashvili@gmail.com>
2023-03-01 13:39:00 +0400

    Implement media controller ui with compose

---
+++ app/src/main/java/com/example/itechart/details_screen/presentation/ui_components/MediaControllerlUI.kt
@@ -0,0 +1,19 @@
+package com.example.itechart.details_screen.presentation.ui_components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MediaController(
+    modifier: Modifier
+) {
+    Row(modifier = modifier
+        .fillMaxWidth()
+        .height(250.dp)) {
+
+    }
+}

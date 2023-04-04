2f6a333afcd7adc0f19832f6f0d84620ef2b1000
Vazhapp <vkentchiashvili@gmail.com>
2023-04-04 15:16:33 +0400

    implemented media controller just UI

---
+++ app/src/main/java/com/example/itechart/details_screen/presentation/DetailsScreen.kt
@@ -0,0 +1,10 @@
+package com.example.itechart.details_screen.presentation
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DetailsScreen(
+
+) {
+
+}
--- app/src/main/java/com/example/itechart/details_screen/presentation/ui_components/MediaControllerlUI.kt
+++ app/src/main/java/com/example/itechart/details_screen/presentation/ui_components/MediaControllerlUI.kt
@@ -1,19 +1,38 @@
 package com.example.itechart.details_screen.presentation.ui_components
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.itechart.R
 
 @Composable
 fun MediaController(
     modifier: Modifier
 ) {
-    Row(modifier = modifier
-        .fillMaxWidth()
-        .height(250.dp)) {
-
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(250.dp)
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.rewind_10_second),
+            contentDescription = "",
+            modifier.size(50.dp)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.play),
+            contentDescription = "",
+            modifier.size(50.dp)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.forward_10_second),
+            contentDescription = "",
+            modifier.size(50.dp)
+        )
     }
 }
